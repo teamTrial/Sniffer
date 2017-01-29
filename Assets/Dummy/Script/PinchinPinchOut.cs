15490635fdc13ed5711b6e2207b2070a6aa4de8a
haratatsurou <hara.tatsurou@gmail.com>
2017-01-30 06:49:05 +0900

    仮素材の総入れ替えとプレイヤーの移動アニメーションの実装

---
+++ Assets/Dummy/Script/PinchinPinchOut.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PinchinPinchOut : MonoBehaviour {
+
+    private float scale;
+    private float backDist = 0.0f;
+
+    // デバッグ用テキスト（scaleサイズ確認用）
+    public Text scaleText;
+
+    // デフォルトサイズ
+    private float defaultScale;
+
+    void Start () {
+        // 初期値取得
+        RectTransform rt = this.GetComponent(typeof (RectTransform)) as RectTransform;
+        defaultScale = rt.sizeDelta.x;
+    }
+
+    void Update () {
+
+        // マルチタッチ判定
+        if (Input.touchCount >= 2) {
+
+            // タッチ開始時に初期値を取得
+            RectTransform rt = this.GetComponent(typeof (RectTransform)) as RectTransform;
+            defaultScale = rt.sizeDelta.x;
+
+            // タッチしている２点を取得
+            Touch touch1 = Input.GetTouch (0);
+            Touch touch2 = Input.GetTouch (1);
+
+            // 2点タッチ開始時の距離を記憶
+            if (touch2.phase == TouchPhase.Began) {
+                backDist = Vector2.Distance (touch1.position, touch2.position);
+            } else if (touch1.phase == TouchPhase.Moved && touch2.phase == TouchPhase.Moved) {
+                // タッチ位置の移動後、長さを再測し、前回の距離からの相対値を取る。
+                float newDist = Vector2.Distance (touch1.position, touch2.position);
+                scale = defaultScale + (newDist - backDist) / 100.0f;
+                scaleText.text = "scalse = " + scale.ToString();
+
+                // 相対値が変更した場合、オブジェクトに相対値を反映させる
+                if(scale != 0) {
+                    UpdateScaling(scale);  
+                }
+            }
+        }
+    }
+
+    /// 設定された拡大率に基づいてオブジェクトの大きさを更新する
+    private void UpdateScaling(float argScale) {
+        RectTransform rt = this.GetComponent(typeof (RectTransform)) as RectTransform;
+        rt.sizeDelta = new Vector2 (argScale, argScale);
+    }
+}
--- Assets/Script/Player/Actoin.cs
+++ Assets/Script/Player/Actoin.cs
@@ -8,12 +8,17 @@ using UnityEngine;
 public class Actoin : MonoBehaviour
 {
     public float Longtap = 1f;
+    float time = 0;
+    bool flag = false;
+    Vector3 old;
+    float counter;
+    Animator anim;
     void Start()
     {
-        hoge();
-        // hoge2( );
+        anim=GetComponent<Animator>();
+        Attack();
     }
-    void hoge()
+    void Attack()
     {
         var doubleclick = this.UpdateAsObservable()
         .Where(_ => Input.GetMouseButtonDown(0));
@@ -22,58 +27,84 @@ public class Actoin : MonoBehaviour
         .Buffer(doubleclick.Throttle(TimeSpan.FromMilliseconds(200)))
         // タップ回数が2回以上だったら処理する
         .Where(tap => tap.Count >= 2)
-        .Subscribe(tap => Debug.Log("Do Something."));
+        .Subscribe(tap => {
+                print("たぶ");
+                setAnimation(3);
+            });
     }
-    /// <summary>
-    /// Update is called every frame, if the MonoBehaviour is enabled.
-    /// </summary>
     void Update()
     {
         CheckLongtap();
+        checkAnim();
+        
     }
-    float time = 0;
-    bool flag = false;
-    Vector3 old;
-    float counter;
     void CheckLongtap()
     {
-
+        
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
+            //タッチしている＆指が動いてない
             if (touch.phase == TouchPhase.Stationary)
             {
                 counter += 0.5f;
                 if (counter > 5f)
                 {
-                    time += 0.1f;
+                    time += 0.5f;
                     if (5f < time)
                     {
-                        print("ロングタップ＋固定");
+                        print("チャージ完了");
                         flag = true;
                         counter = 0;
                     }
                 }
             }
+            //タッチしている＆指が動いている
             else if (touch.phase == TouchPhase.Moved)
             {
                 counter += 0.5f;
                 if (counter > 5f)
                 {
-                    print("移動したのでキャンセル");
+                    // print("移動したのでキャンセル");
+                    //初期化----------------
                     time = 0;
                     flag = false;
                     counter = 0;
+                    //---------------------
                 }
             }
+            //タッチしている指が離れた
             else if (touch.phase == TouchPhase.Ended)
             {
                 if (flag)
                 {
                     print("構えるモーション");
+                    setAnimation(2);
                 }
+                time=0;
                 flag = false;
                 counter = 0;
+                return;
+            }
+            setAnimation(1);
+        }
+    }
+    /// <summary>
+    ///アニメーションを再生させる
+    ///0=止まる
+    ///1=歩く
+    ///2=構える
+    ///3=攻撃
+    /// </summary>
+    void setAnimation(int animnum=0){
+        anim.SetInteger("Anim",animnum);
+    }
+    void checkAnim(){
+        AnimatorStateInfo animInfo = anim.GetCurrentAnimatorStateInfo(0);
+        if(1.0f<animInfo.normalizedTime )
+        {
+            if(1<anim.GetInteger("Anim")){
+                setAnimation(0);
             }
         }
     }
