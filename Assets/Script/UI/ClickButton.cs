3de7745746cb5d5bbd089060e1d1c41031c494c2
haratatsurou <hara.tatsurou@gmail.com>
2017-02-07 17:36:19 +0900

    スタート画面の実装

---
+++ Assets/Script/UI/ClickButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickButton : MonoBehaviour {
+    public string sence="globalSelect";
+    void Start () {
+        Button	Button=this.GetComponent<Button>();
+        var startclick=Button.onClick.AsObservable();
+        startclick
+        .Take(1)
+        .Subscribe(tap => {
+            FadeManager.Instance.LoadLevel(sence,2f);
+        });
+    }
+}
