5b7b3d2fd125aa6219d0542db7b41d452a53c607
haratatsurou <hara.tatsurou@gmail.com>
2017-02-16 18:26:39 +0900

    仕様変更適応版 中央のボタンを長押しすると(1秒)出力するように

--- Assets/Script/Player/Actoin.cs
+++ Assets/Script/Player/Actoin.cs
@@ -5,74 +5,75 @@ using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.UI;
-public class Actoin : MonoBehaviour
-{
+public class Actoin : MonoBehaviour {
     Animator anim;
     public Button center;
     //攻撃中は移動できないように
-    public static bool attackFlag{
+    public static bool attackFlag {
         get;
         private set;
     }
-    void Start()
-    {
-        anim=GetComponent<Animator>();
-        Attack();
+    void Start () {
+        anim = GetComponent<Animator> ();
+        Attack ();
     }
-    void Attack()
-    {
-        var singleclick=center.onClick.AsObservable();
-        singleclick
-        .Buffer(singleclick.Throttle(TimeSpan.FromMilliseconds(200)))
-        .Where(tap=> 1 >= tap.Count)
-        .Subscribe(tap => {
-            attackFlag=true;
-            setAnimation(2);
-        });
-        
-        var doubleclick =center.onClick.AsObservable();
+    void Attack () {
+        // var singleclick=center.onClick.AsObservable();
+        // singleclick
+        // .Buffer(singleclick.Throttle(TimeSpan.FromMilliseconds(200)))
+        // .Where(tap=> 1 >= tap.Count)
+        // .Subscribe(tap => {
+        //     attackFlag=true;
+        //     setAnimation(2);
+        // });
+
+        var doubleclick = center.onClick.AsObservable ();
         doubleclick
-        // 0.2秒以内のメッセージをまとめる
-        .Buffer(doubleclick.Throttle(TimeSpan.FromMilliseconds(300)))
-        // タップ回数が2回以上だったら処理する
-        .Where(tap => tap.Count >= 2)
-        .Subscribe(tap => {
-            attackFlag=true;
-            setAnimation(3);
-        });
-        var longtap=this.UpdateAsObservable();
-        longtap.TakeUntil(doubleclick).RepeatSafe()
-        .Subscribe(_=>{
-            CheckLongtap();
-        });
+            // 0.2秒以内のメッセージをまとめる
+            .Buffer (doubleclick.Throttle (TimeSpan.FromMilliseconds (300)))
+            // タップ回数が2回以上だったら処理する
+            .Where (tap => tap.Count >= 2)
+            .Subscribe (tap => {
+                attackFlag = true;
+                setAnimation (3);
+            });
+        var longtap = this.UpdateAsObservable ();
+        longtap.TakeUntil (doubleclick).RepeatSafe ()
+            .Subscribe (_ => {
+                CheckLongtap ();
+            });
     }
-    void LateUpdate()
-    {
-        checkAnim();
+    void LateUpdate () {
+        checkAnim ();
     }
-    void CheckLongtap()
-    {
-        
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            if(!attackFlag){
+    float hoge = 0;
+    void CheckLongtap () {
+
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch (0);
+            // print (hoge);
+            if (!attackFlag) {
                 //タッチしている＆指が動いてない
-                if (touch.phase == TouchPhase.Stationary)
-                {
-                    setAnimation(1);
+                if (touch.phase == TouchPhase.Stationary) {
+                    hoge += Time.deltaTime;
+                    // setAnimation(1);
                 }
                 //タッチしている＆指が動いている
-                else if (touch.phase == TouchPhase.Moved)
-                {
-                    setAnimation(1);
+                else if (touch.phase == TouchPhase.Moved) {
+                    setAnimation (1);
+                    hoge += Time.deltaTime;
                 }
             }
             //タッチしている指が離れた
-            if (touch.phase == TouchPhase.Ended)
-            {
-                if(anim.GetInteger("Anim")<2){
-                    setAnimation(0);
+            if (touch.phase == TouchPhase.Ended) {
+                if (PlayerController.CenterFlag) {
+                    if (hoge > 1.0f) {
+                        print ("襲うアクション");
+                    }
+                }
+                hoge = 0;
+                if (anim.GetInteger ("Anim") < 2) {
+                    setAnimation (0);
                 }
             }
         }
@@ -84,17 +85,16 @@ public class Actoin : MonoBehaviour
     ///2=構える
     ///3=攻撃
     /// </summary>
-    void setAnimation(int animnum=0){
-        anim.SetInteger("Anim",animnum);
+    void setAnimation (int animnum = 0) {
+        anim.SetInteger ("Anim", animnum);
     }
-    void checkAnim(){
-        AnimatorStateInfo animInfo = anim.GetCurrentAnimatorStateInfo(0);
-        if(1.0f<animInfo.normalizedTime )
-        {
+    void checkAnim () {
+        AnimatorStateInfo animInfo = anim.GetCurrentAnimatorStateInfo (0);
+        if (1.0f < animInfo.normalizedTime) {
             //アニメーションが止まると移動以外だった場合
-            if(1<anim.GetInteger("Anim")){
-                setAnimation(0);
-                attackFlag=false;
+            if (1 < anim.GetInteger ("Anim")) {
+                setAnimation (0);
+                attackFlag = false;
             }
         }
     }
--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -6,33 +6,38 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
-    
+
     private Vector2 Center;
     public GameObject Player;
+    public static bool CenterFlag { get; private set; }
     /// <summary>
     /// 右向きtrue;左向きfalse
     /// </summary>
     public static bool PlayerDirectoin { get; private set; }
     void Start () {
         PlayerDirectoin = true;
-        GetDistancefromPovittoFinger( );
+        CenterFlag=false;
+        GetDistancefromPovittoFinger ();
     }
     /// <summary>
     /// 中心地から指の距離を毎フレーム算出する
     /// </summary>
-    void GetDistancefromPovittoFinger() {
-        this.UpdateAsObservable( )
-        .TakeWhile(clearFlag=>!StageClear.ClearFlag)
-        .Where(distance=>Input.GetMouseButton(0))
-        .Select(distance=> {
-                Center = GetPovit( );
-                return FingerPos(Input.mousePosition);
+    void GetDistancefromPovittoFinger () {
+        this.UpdateAsObservable ()
+            .TakeWhile (clearFlag => !StageClear.ClearFlag)
+            .Where (distance => Input.GetMouseButton (0))
+            .Select (distance => {
+                Center = GetPovit ();
+                return FingerPos (Input.mousePosition);
             })
-        .Subscribe(distance => {
-                Vector2 dis = new Vector2(distance.x-Center.x,0);
-                if(!(-0.6f<dis.x&&dis.x<0.6f)&&!Actoin.attackFlag){
-                    Direction(dis);
-                    Player.transform.Translate(dis*Time.deltaTime);
+            .Subscribe (distance => {
+                Vector2 dis = new Vector2 (distance.x - Center.x, 0);
+                if (!(-1.6f < dis.x && dis.x < 1.6f) && !Actoin.attackFlag) {
+                    Direction (dis);
+                    Player.transform.Translate (dis * Time.deltaTime);
+                    CenterFlag=false;
+                }else if(-1.6f < dis.x && dis.x < 1.6f){
+                    CenterFlag=true;
                 }
             });
     }
@@ -43,7 +48,7 @@ public class PlayerController : MonoBehaviour {
     /// <returns>
     /// 画像のワールド座標(Povit)
     /// </returns>
-    Vector2 GetPovit() {
+    Vector2 GetPovit () {
         var getpovit = this.transform.position;
         return getpovit;
     }
@@ -57,11 +62,11 @@ public class PlayerController : MonoBehaviour {
     /// <returns>
     /// 指のワールド座標(Vector2)
     /// </returns>
-    Vector2 FingerPos(Vector2 fingerpos) {
-        fingerpos = Camera.main.ScreenToWorldPoint(fingerpos);
+    Vector2 FingerPos (Vector2 fingerpos) {
+        fingerpos = Camera.main.ScreenToWorldPoint (fingerpos);
         //1-------------------------------------------------------------
-        var Height = GetComponent<RectTransform>( ).sizeDelta.y;
-        if ( Height < Input.mousePosition.y ) {
+        var Height = GetComponent<RectTransform> ().sizeDelta.y;
+        if (Height < Input.mousePosition.y) {
             return Center;
         }
         //--------------------------------------------------------------
@@ -73,21 +78,22 @@ public class PlayerController : MonoBehaviour {
     /// <param name="dis">
     /// 指と中央の差分
     /// </param>
-    void Direction(Vector2 dis) {
+    void Direction (Vector2 dis) {
         var x = Player.transform.localScale.x;
         //右向き
-        if ( 0<dis.x  ) {
+        if (0 < dis.x) {
             PlayerDirectoin = true;
-            x = Mathf.Abs(Player.transform.localScale.x);
+            x = Mathf.Abs (Player.transform.localScale.x);
         }
         //左向き
-        else if ( dis.x <0) {
+        else if (dis.x < 0) {
             PlayerDirectoin = false;
-            x = -Mathf.Abs(Player.transform.localScale.x);
+            x = -Mathf.Abs (Player.transform.localScale.x);
         }
-        Player.transform.localScale = new Vector3(
+        Player.transform.localScale = new Vector3 (
             x,
             Player.transform.localScale.y,
-            Player.transform.localScale.z);
+            Player.transform.localScale.z
+        );
     }
 }
