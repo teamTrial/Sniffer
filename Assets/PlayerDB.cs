06049d7ccaf751250d6d95d48c1324d6006f3a76
haratatsurou <hara.tatsurou@gmail.com>
2017-03-25 05:40:13 +0900

    規定値を上回ってしまった場足それ以下の値にならないように

--- Assets/PlayerDB.cs
+++ Assets/PlayerDB.cs
@@ -12,11 +12,17 @@ public class PlayerDB : SingletonMonoBehaviour<PlayerDB> {
             return GameObject.Find ("UI/HP").GetComponent<Image> ();
         }
     }
-    public Image MP_ui {
+
+    public Image AP_ui {
         get {
             return GameObject.Find ("UI/MP").GetComponent<Image> ();
         }
     }
+    /// <summary>
+    /// AttentionPoint=AP 注目度に関するポイント
+    /// </summary>
+    public float AP = 0;
+    private float OldAP = 0;
     GameObject cameraShake;
     // Status Player = new Status ("Player",15);
     public float HP = 15;
@@ -27,13 +33,12 @@ public class PlayerDB : SingletonMonoBehaviour<PlayerDB> {
 
     void Start () {
         OldHP = HP;
-        Heel (0);
+#if UNITY_EDITOR
+        // Heel (0);
+#endif
         cameraShake = Camera.main.gameObject;
     }
-    /// <summary>
-    /// Update is called every frame, if the MonoBehaviour is enabled.
-    /// </summary>
-    void Update () { }
+
     public void Heel (int interval = 20) {
         this.UpdateAsObservable ()
             .TakeWhile (NotCentor => !PlayerController.BattleFlag)
@@ -42,14 +47,7 @@ public class PlayerDB : SingletonMonoBehaviour<PlayerDB> {
                 SyncHP (true);
             }).AddTo (this.gameObject);
     }
-    public void Death () {
-        this.UpdateAsObservable ()
-            .Where (HP => HP_ui.fillAmount == 0)
-            .First ()
-            .Subscribe (_ => {
-                print ("プレイヤー死亡");
-            }).AddTo (this.gameObject);
-    }
+
     public void Damage (String EnemyName, int EnemyHP) {
         iTween.ShakePosition (cameraShake, new Vector2 (0.1f, 0.1f), 0.3f);
         var random = UnityEngine.Random.Range (0.005f, 0.02f);
@@ -62,19 +60,39 @@ public class PlayerDB : SingletonMonoBehaviour<PlayerDB> {
     /// <param name="Mode">true=回復,false=ダメージ</param>
     void SyncHP (bool Mode, float damage = 0) {
         if (Mode) {
-            HP_ui.fillAmount += 0.01f * Time.deltaTime;
-            MP_ui.fillAmount -= 0.005f * Time.deltaTime;
+            var heelPoint = 0.01f * Time.deltaTime;
+            HP_ui.fillAmount += heelPoint;
+            AttentionPointSystem (heelPoint);
         } else {
             HP_ui.fillAmount -= damage;
-            MP_ui.fillAmount += damage;
+            AP_ui.fillAmount += damage;
+            OldAP = AP;
+            if (HP <= 0) {
+                Death ();
+            }
         }
+        print ("OldAP:"+OldAP);
+        AP = AP_ui.fillAmount * 5;
         HP = HP_ui.fillAmount * OldHP;
+    }
+    /// <summary>
+    /// OldAPが1になった場合最低値が1になるように
+    /// OldAPが2になった場合最低値が2になるように
+    /// OldAPが3になった場合最低値が3になるように
+    /// </summary>
+    void AttentionPointSystem (float heelPoint) {
+        if ((int) OldAP < AP) {
+            AP_ui.fillAmount -= heelPoint;
+        }
     }
     public void getExp (float EnemyExp) {
         Exp += EnemyExp;
         if (Lv <= Exp) {
             LevelUp ();
         }
+    }
+    void Death () {
+        print ("プレイヤー死亡");
     }
     void LevelUp () {
         Lv++;
