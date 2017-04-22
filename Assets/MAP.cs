908b1defb2d96396ed366c861919391ad4536832
haratatsurou <hara.tatsurou@gmail.com>
2017-04-22 22:08:49 +0900

    引数を渡すとステージがアンロックするように

---
+++ Assets/MAP.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MAP : MonoBehaviour {
+	public string stagenum;
+	void Start()
+	{
+	ActiveStage(3);	
+	}
+	public void ActiveStage(int stagenum=1){
+		//ステージのアクティブ化
+		GameObject.Find("Canvas").transform.Find("stage"+stagenum).gameObject.SetActive(true);
+	}
+}
